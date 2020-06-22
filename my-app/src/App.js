6fe330082aa09bd6cf3e32a1075db8e2f2f1af90
MY2333 <66840042+MY2333@users.noreply.github.com>
2020-06-22 17:40:32 -0400

    session2
    Homework for session 2

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,26 +1,49 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import check from './Change.js'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload ^_^.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component{
+  constructor(){
+    super();
+    this.state = {
+      count: 0,
+      message: "^_^"
+    }
+  }
+  
+  render(){
+
+    const { count, message }= this.state;
+    const flag = check(count);
+    var a = null;
+    if(flag === true){
+      a = "QwQ";
+    }else{
+      a = "o(^_^)o";
+    }
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Nice to see you {message}.
+          </p>
+            
+          <a
+            className="App-link"
+            href="https://www.google.com/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Google
+          </a>
+          <button onClick={()=> this.setState({count: count+1, message:a})}>Change</button>
+        </header>
+      </div>
+    );
+  }
 }
+
 
 export default App;
