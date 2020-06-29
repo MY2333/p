2ac1a9cd763fd86b323dd39cecada9e374b427aa
MY2333 <66840042+MY2333@users.noreply.github.com>
2020-06-29 17:05:41 -0400

    web1

---
+++ Web/my-app/src/App.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div className="fixed">
+        <img src="https://www.google.com/images/branding/googlelogo/1x/googlelogo_color_272x92dp.png" 
+        alt="test"/>
+        <h1 className="App-h1">
+          Margaret Yao
+        </h1>
+      </div>
+      <header class="App-header">
+        Background
+      </header>
+      <ul className="App-ul">
+        <li>Second year student in UofT</li>
+        <li>abc</li>
+        <li>ABC</li>
+      </ul>
+      <header className="App-header">Experience</header>
+      <ul className="App-ul">
+        <li>1234</li>
+        <li>abcd</li>
+        <li>ABCD</li>
+      </ul>
+    </div>
+  );
+}
+
+export default App;
