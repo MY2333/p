ad4fee3435c844d696cd5087df4a0670f924d77b
MY2333 <66840042+MY2333@users.noreply.github.com>
2020-07-27 19:14:39 -0400

    hw11

--- my-app/src/PageA.js
+++ my-app/src/PageA.js
@@ -1,11 +1,19 @@
 import React, {useState, useEffect} from 'react';
 import './App.css';
+import axios from 'axios';
 
 
 const PageA = (props) => {
     const [smile, setSmile] = useState("^_^");
+    const [w, setW] = useState();
+    useEffect(() => {
+        setW("Hi")
+    })
     return (
-        <div className = "App" onClick={() => setSmile("o(^_^)o")}>page1{smile}</div>
+        <div>
+            <div className = "App" onClick={() => setSmile("o(^_^)o")}>page1{smile}</div>
+            <div>{w}</div>
+        </div>
     )
 }
 export default PageA;
